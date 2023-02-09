6d953f931a750e036b5d0d1f0360c78a42fdb1dc
javascriptsucks <evix1988@live.cn>
2023-02-09 16:23:38 -0600

    refactor-get-all-revervations-function

--- server/database.js
+++ server/database.js
@@ -97,7 +97,32 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const allReservationQuery = `
+    SELECT reservations.*, properties.*, AVG(rating) as average_rating
+    FROM reservations
+
+    JOIN properties
+    ON reservations.property_id = properties.id
+    JOIN property_reviews
+    ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2
+  ;
+  `;
+  const sqlParams = [guest_id, limit];
+
+  return pool.query(allReservationQuery, sqlParams)
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    }).catch(err => {
+      console.error(err.message);
+    });
+
+  // return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
 
